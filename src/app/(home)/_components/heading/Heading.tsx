e180b5826af87a4bf17b96c6a75fb994713a76d4
krckyboy <dusan.todorovic.2016@gmail.com>
2024-08-31 08:04:49 +0200

    fix: container -> div

--- src/app/(home)/_components/heading/Heading.tsx
+++ src/app/(home)/_components/heading/Heading.tsx
@@ -6,10 +6,10 @@ interface Props {
 
 const Heading: FunctionComponent<Props> = () => {
   return (
-    <container className={styles.container}>
+    <div className={styles.container}>
       <h1>Generate Project</h1>
       <p>Taskify will help you set things up</p>
-    </container>
+    </div>
   );
 };
 
