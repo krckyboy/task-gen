6756ef0c4d30ed14b0b3b34f226e7299a6ea16c0
krckyboy <dusan.todorovic.2016@gmail.com>
2024-09-08 10:15:53 +0200

    feat: Added new logic to Technologies.

--- src/components/form/technologies/Technologies.tsx
+++ src/components/form/technologies/Technologies.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 interface Props {
 }
@@ -13,9 +14,14 @@ const Technologies: React.FC<Props> = () => {
   };
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter' && inputValue.trim() !== '') {
+    if (e.key === 'Enter' || e.key === ' ' && inputValue.trim() !== '') {
       // Prevent form submission
       e.preventDefault();
+
+      if (tags.find((tag) => tag === inputValue.trim())) {
+        setInputValue('');
+        return;
+      }
 
       setTags([...tags, inputValue.trim()]);
 
@@ -24,19 +30,43 @@ const Technologies: React.FC<Props> = () => {
     }
   };
 
+  const handleBlur = () => {
+    if (inputValue.trim() !== '') {
+      setTags([...tags, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  const removeTag = (tagToRemove: string) => {
+    setTags(tags.filter(tag => tag !== tagToRemove));
+  };
+
   return (
     <div>
       <label htmlFor={'technologies'} className={styles.label}>
         <span>Technologies</span>
         <div className={styles.tagsContainer}>
           {tags.map((tag, index) => (
-            <span key={index} className={styles.tag}>{tag}</span>
+            <div key={index} className={styles.tag}>
+              <span>{tag}</span>
+              <button
+                type="button"
+                onClick={() => removeTag(tag)}
+                className={styles.cross}
+              >
+                <Image alt={'X'}
+                       width={14}
+                       height={14}
+                       src={'/images/cross.svg'}
+                /></button>
+            </div>
           ))}
         </div>
         <input
           type={'text'}
           onChange={handleInputChange}
           onKeyDown={handleKeyPress}
+          onBlur={handleBlur}
           value={inputValue}
         />
       </label>
