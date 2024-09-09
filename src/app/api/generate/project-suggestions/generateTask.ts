b1f857c9db876d9b94f301282965caad605a3d7f
krckyboy <dusan.todorovic.2016@gmail.com>
2024-09-09 19:01:57 +0200

    feat: #8 Generating up to 10.

--- src/app/api/generate/project-suggestions/generateTask.ts
+++ src/app/api/generate/project-suggestions/generateTask.ts
@@ -23,7 +23,7 @@ export async function generateTasks(formData: FormData): Promise<string> {
         {
           'role': 'system',
           'content': `Your task is to generate a task/project based on the provided criteria as you are to help developers of different experience to come up with something to work on. 
-          Generate 3 projects with a preview of description, so on the next step they will choose one of the options so you can continue generating it all. 
+          Generate up to 10 projects (if you are out of good ideas, you can generate less than 10) with a preview of description, so on the next step they will choose one of the options so you can continue generating it all. 
           Just straight to the point, and let the response by in JSON, so I can just JSON.parse it easily in the API, so no need for MD, just stringified JSON. 
           The projects should be generated in a way so that they are suited for the technologies provided to you.
           In the description, don't focus so much on the technologies, focus more on what the apps are about in a general manner, what does it do and how it helps its users, but also make sure so you DO mention how technologies come in the play.
