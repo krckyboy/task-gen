648ed66418c3f73ef6a90f828a251da8e156d0bc
krckyboy <dusan.todorovic.2016@gmail.com>
2024-09-09 07:02:45 +0200

    feat: Additional style and logic for range input.

--- src/components/form/range-input/RangeInput.tsx
+++ src/components/form/range-input/RangeInput.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import styles from './styles.module.scss';
 
 interface Props {
@@ -7,11 +7,34 @@ interface Props {
 }
 
 const RangeInput: FunctionComponent<Props> = ({ name, label }) => {
+  const [value, setValue] = useState(50);
+  const [style, setStyle] = useState({
+    background: `linear-gradient(to right, #DC5F00 ${50}%, #373A40 ${50}%)`
+  });
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value, 10);
+    setValue(newValue);
+
+    const progress = (newValue / 100) * 100;
+
+    setStyle({
+      background: `linear-gradient(to right, #DC5F00 ${String(progress)}%, #373A40 ${String(progress)}%)`
+    });
+  }, []);
+
   return (
     <>
       <label htmlFor={name}>
         <span>{label}</span>
-        <input className={styles.input} id={name} name={name} type={'range'} />
+        <span>{value}</span>
+        <input onChange={handleChange}
+               className={styles.input}
+               id={name}
+               name={name}
+               type={'range'}
+               style={style}
+        />
       </label>
     </>
   );
