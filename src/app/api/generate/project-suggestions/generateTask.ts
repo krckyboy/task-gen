7269057b6af5842a39f9a58555ae870036a08643
krckyboy <dusan.todorovic.2016@gmail.com>
2024-09-15 20:46:25 +0200

    feat: Tweaking the query.

--- src/app/api/generate/project-suggestions/generateTask.ts
+++ src/app/api/generate/project-suggestions/generateTask.ts
@@ -21,13 +21,25 @@ export async function generateTasks(formData: FormData): Promise<string> {
       messages: [
         {
           'role': 'system',
-          'content': `Your task is to generate a task/project based on the provided criteria as you are to help developers of different experience to come up with something to work on. 
-          Generate up to 10 projects (if you are out of good ideas, you can generate less than 10) with a preview of description, so on the next step they will choose one of the options so you can continue generating it all. 
-          Just straight to the point, and let the response by in JSON, so I can just JSON.parse it easily in the API, so no need for MD, just stringified JSON. 
+          'content': `
+          Your task is to generate a task/project based on the provided criteria as you are to help developers of 
+          different experience to come up with something to work on. 
+          
+          Generate no more than 4 projects with a preview of description (with the biggest variety being those 4).
+          
+          Let the response by in JSON, so I can just JSON.parse it easily in the API, so no need for MD, just stringified JSON. 
+          
           The projects should be generated in a way so that they are suited for the technologies provided to you.
-          In the description, don't focus so much on the technologies, focus more on what the apps are about in a general manner, what does it do and how it helps its users, but also make sure so you DO mention how technologies come in the play.
-          Make sure to pay attention to complexity of the project, since it goes like: beginner: 0-25; intermediate: 26-50; advanced: 51-75; expert: 76-100.
-          Also, return the data in the format of an object with projects property. It should have description and title property.`
+          
+          In the description, don't focus so much on the technologies, focus more on what the apps are about in a general manner, 
+          as if you are explaining it to a project manager - what does it do and how it helps its users,
+          but also make sure so you take into account that technologies mentioned are suited for the project.
+          
+          Make sure to pay attention to the complexity of the project, since it goes like: beginner: 0-25; 
+          intermediate: 26-50; advanced: 51-75; expert: 76-100.
+          
+          Also, return the data in the format of an object with projects property. It should have description and title property.
+          `
         },
         {
           'role': 'user',
