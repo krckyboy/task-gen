14bcba3873be6c74a26bda50185563b7f70e0b8d
krckyboy <dusan.todorovic.2016@gmail.com>
2024-09-18 21:12:38 +0200

    feat: Adding header and footer blank components.

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+}
+
+const Footer: FunctionComponent<Props> = () => {
+  return (
+    <footer></footer>
+  );
+};
+
+export default Footer;
