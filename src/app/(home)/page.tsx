29f26ba2675f895c806908bf2e0970a48b206317
Dušan Todorović <dusan.todorovic.2016@gmail.com>
2024-09-19 18:44:49 +0200

    Revert "#16"

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,13 @@
 import Heading from '@/app/(home)/_components/heading/Heading';
 import Form from '@/components/form/Form';
 import Projects from '@/components/projects/Projects';
-import styles from './styles.module.scss';
 
 export default function Home() {
   return (
     <main>
       <Heading />
-      <div className={styles.formProjectsContainer}>
-        <Form />
-        <Projects />
-      </div>
+      <Form />
+      <Projects />
     </main>
   );
 }
